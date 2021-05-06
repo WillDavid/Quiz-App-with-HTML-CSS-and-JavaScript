cecde49cd8cf79cfcb3f348be00a75c84a0c667a
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-05-05 20:31:33 -0400

    update game.js

--- src/game.js
+++ src/game.js
@@ -2,6 +2,8 @@
 
 const question = document.getElementById("question");
 
+
+
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 
@@ -19,29 +21,78 @@ let questions = [
         choice2: "Naruto",
         choice3: "Rock Lee",
         choice4: "Sakura",
-        answer: 1
+        answer: 2
         
     },
 
     {
-        question: "Qual é o nome do personagem principal de Naruto?",
-        choice1: "Kakashi",
-        choice2: "Naruto",
-        choice3: "Rock Lee",
-        choice4: "Sakura",
-        answer: 1
+        question: "Qual é o nome do personagem principal de DragonBall?",
+        choice1: "Minato",
+        choice2: "Goku",
+        choice3: "Ichigo",
+        choice4: "Naruto",
+        answer: 2
         
     }
-]
+];
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
-function startGame(){
+startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions)
+    availableQuestions = [ ... questions];
+    getNewQuestion();
 }
+
+getNewQuestion = () => {
+
+    if(availableQuestions.length === 0 | questionCounter >= MAX_QUESTIONS){
+        return window.location.assign('/end.html');
+    }
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+
+
+
+    currentQuestion = availableQuestions[questionIndex];
+
+
+    question.innerText = currentQuestion.question;
+
+
+    
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+
+
+ 
+
+
+    
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        console.log(selectedAnswer === currentQuestion.answer)
+        getNewQuestion();
+    })
+})
+
 
 startGame();
