4474cb70b9251f5a3da87501e6ca092a64419647
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-05-09 13:50:07 -0400

    create end js

---
+++ src/end.js
@@ -0,0 +1,19 @@
+const username = document.getElementById('username');
+
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+
+const finalScore = document.getElementById('finalScore');
+
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentScore;
+
+
+username.addEventListener('keyup', () => {
+
+    saveScoreBtn.disabled = !username.value;
+})
+saveHighScore = (e) => {
+    console.log("Acontece algo")
+    e.preventDefault();
+}
--- src/game.js
+++ src/game.js
@@ -57,6 +57,8 @@ startGame = () => {
 getNewQuestion = () => {
 
     if(availableQuestions.length === 0 | questionCounter >= MAX_QUESTIONS){
+
+        localStorage.setItem('mostRecentScore', score);
         return window.location.assign('end.html');
     }
 
